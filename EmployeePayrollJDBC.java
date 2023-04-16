2ab47af2fda6a25b975f9475e03514b2dab5ec78
sanjana8100 <sanjanams812@gmail.com>
2023-04-16 21:46:23 +0530

    added: UC1_JDBC

---
+++ EmployeePayrollJDBC.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.EmployeePayrollProblem;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class EmployeePayrollJDBC {
+    static final String DB_URL = "jdbc:mysql://localhost:3306/payroll_service";
+    static final String USER = "root";
+    static final String PASS = "Admin";
+
+    public static void main(String[] args) {
+        Connection con;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded!");
+        } catch (ClassNotFoundException e){
+            throw new IllegalStateException("Cannot find the driver in the classpath!",e);
+        }
+
+        listDrivers();
+
+        try {
+            System.out.println("Connecting to database: "+DB_URL);
+            con = DriverManager.getConnection(DB_URL,USER,PASS);
+            System.out.println("Connection is successful!!!! "+con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass = driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
+    }
+}
